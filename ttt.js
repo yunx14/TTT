d8d30ba82c92fee33ff17b5f007888bf72671910
charles johnston <charlesjohnston@charless-MacBook-Air.local>
2017-11-30 07:33:35 -0800

    1st finished prototype

--- ttt.js
+++ ttt.js
@@ -12,8 +12,6 @@ var TTT = (function() {
     }
 
     var init = function() {
-    	console.log('starting...');
-    	console.log('first append the board to the screen.');
     	loadCSS(stylesheet);
     	createBoard();
     }
@@ -35,17 +33,12 @@ var TTT = (function() {
         for (var i = 0; i < squaresArr.length; i++) {
             squares.push(new Square(i+1));
         }
-        console.log("Creating squares objects... ");
     }
 
     function clickHandler(e) {
 		if (e.target !== e.currentTarget) {
 	        var clickedItem = e.target.id.slice(6);
-	        console.log("Getting the id of clicked square...");
             squares[clickedItem-1].setValue(player);
-            console.log("setting value of square "+ clickedItem + " to " + player);
-            switchPlayer();
-            console.log(squares);
 	    }
 		e.stopPropagation();
 	}
@@ -58,21 +51,51 @@ var TTT = (function() {
         }
     }
 
+    function checkForWinner() {
+        console.log(squares);
+        var lines = [];
+        lines[0] = squares[0]._numVal + squares[1]._numVal + squares[2]._numVal;
+        lines[1] = squares[3]._numVal + squares[4]._numVal + squares[5]._numVal;
+        lines[2] = squares[6]._numVal + squares[7]._numVal + squares[8]._numVal;
+        lines[3] = squares[0]._numVal + squares[3]._numVal + squares[6]._numVal;
+        lines[4] = squares[1]._numVal + squares[4]._numVal + squares[7]._numVal;
+        lines[5] = squares[2]._numVal + squares[5]._numVal + squares[8]._numVal;
+        lines[6] = squares[0]._numVal + squares[4]._numVal + squares[8]._numVal;
+        lines[7] = squares[2]._numVal + squares[4]._numVal + squares[6]._numVal;
+
+        for (var i = 0; i < lines.length; i++) {
+            if (lines[i] == 3){
+                console.log("X wins");
+            } else if (lines[i] == -3) {
+                console.log("O wins");
+            }
+        }
+    }
+
     class Square {
-        constructor(id, empty, value) {
+        constructor(id, empty, numVal, value) {
             this._id = id;
             this._empty = true;
             this._value = '';
+            this._numVal = 0;
         }
 
         setValue(value) {
             if(this._empty == true) {
                 this._value = value;
+                this._empty = false;
+                if (value == "X") {
+                    this._numVal = 1;
+                } else {
+                    this._numVal = -1;
+                }
+                switchPlayer();
             } else {
                 return false;
             }
 
             document.getElementById("square"+this._id).innerHTML = this._value;
+            checkForWinner();
         }
 
     }
